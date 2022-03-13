2ee7b85944557141c62bb0ee3e658e0ea324943f
squiddude38 <squiddude38@gmail.com>
2022-03-13 19:56:31 +1300

    started login auth code

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, url_for, g
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+import secrets
+
 
 app = Flask(__name__)
+# secret key
+app.secret_key = secrets.token_urlsafe(32)
+
+
+# using the database
+DATABASE = "FootballHero.db"
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 @app.route("/")
@@ -16,6 +40,18 @@ def signup():
 @app.route("/signin")
 def signin():
     return render_template('signin.html')
+
+
+@app.route("/signup_post", methods=['POST'])
+def signup_post():
+
+    username = str(request.form.get('username'))
+    email = str(request.form.get('email'))
+
+    if (str(request.form.get('password1'))) != (str(request.form.get('password2'))):
+        flash('passwords dont match')
+        return redirect((url_for('signup')))
+    return redirect((url_for('home')))
 
 
 if __name__ == "__main__":
