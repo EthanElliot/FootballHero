18dd2e25a33bce8d97b2e092586d0e157c37d0a3
squiddude38 <squiddude38@gmail.com>
2022-03-14 11:55:54 +1300

    continued development on sign up feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, url_for, g
+from flask import Flask, flash, redirect, render_template, request, url_for, g,session
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import secrets
@@ -42,16 +42,62 @@ def signin():
     return render_template('signin.html')
 
 
+@app.route("/dashboard")
+def dashboard():
+    
+    if 'user' in session:
+        user = session['user']
+        print(user)
+        return render_template('dashboard.html', username = user)
+
+    else:
+        return redirect((url_for('signin')))
+
+
+
 @app.route("/signup_post", methods=['POST'])
 def signup_post():
 
-    username = str(request.form.get('username'))
-    email = str(request.form.get('email'))
+    if 'user' in session:
+        return redirect((url_for('dashboard')))
 
-    if (str(request.form.get('password1'))) != (str(request.form.get('password2'))):
-        flash('passwords dont match')
-        return redirect((url_for('signup')))
-    return redirect((url_for('home')))
+    else:
+        username = str(request.form.get('username'))
+        email = str(request.form.get('email'))
+
+        if (str(request.form.get('password1'))) != (str(request.form.get('password2'))):
+            flash('passwords dont match')
+            return redirect((url_for('signup')))
+        if len(str(request.form.get('password1'))) < 2:
+            flash('password is too short')
+            return redirect((url_for('signup')))
+
+        hashed_password = generate_password_hash(str(request.form.get('password2')))
+
+
+        #sql for email and username check
+        cursor =get_db().cursor()
+        sql = f"""select * from User 
+                    where email = "{email}" OR username = "{username}"; """
+        cursor.execute(sql)
+        results1 = cursor.fetchall()
+
+        if len(results1) > 0:
+            flash('username or email already in user')
+            return redirect((url_for('signup')))
+
+        else:
+            cursor = get_db().cursor()
+            sql = f"""insert into user(username, email, password)
+                        values ("{username}", "{email}", "{hashed_password}")"""
+            cursor.execute(sql)
+            get_db().commit()
+
+            
+            session['user'] = username
+            return redirect((url_for('dashboard')))
+
+        
 
 
 if __name__ == "__main__":
