4ce2a390ef3f5440a49efe99a223b49a490d9e65
squiddude38 <squiddude38@gmail.com>
2022-06-03 16:57:40 +1200

    added the selection of exercises to create modal

--- website/static/javascript/dashboard-create.js
+++ website/static/javascript/dashboard-create.js
@@ -27,6 +27,8 @@ var modalmessage = $("#dashboard-create-modal-message");
 //modal template
 var modaltemplate = $("#dashboard-create-modal-template")[0];
 
+selectedexercise = [];
+
 modalsearch.click(function () {
   // remove all elements from the div
   modalbody
@@ -52,8 +54,15 @@ modalsearch.click(function () {
 
   //send a post request for the data
   get_exercises(textval, selectval).then((responce) => {
-    //log the responce (testing)
-    console.log(responce);
+    //have selected exercises added to the frount of the list
+    if (selectedexercise.length !== 0) {
+      for (var i = 0; i < responce.length; i++) {
+        if (responce[i][0] === selectedexercise[0]) {
+          responce.splice(i, 1);
+        }
+      }
+      responce.unshift(selectedexercise);
+    }
 
     //make the loader invisable as we have the responce
     modalloader.css("display", "none");
@@ -63,8 +72,6 @@ modalsearch.click(function () {
     } else {
       modalbody.removeClass("dashboard-create-modal-exercise-center");
       for (var i = 0; i < responce.length; i++) {
-        console.log(responce[i]);
-
         //clone the html template
         var exercise_clone = modaltemplate.content.cloneNode(true);
 
@@ -76,9 +83,49 @@ modalsearch.click(function () {
           "#dashboard-create-modal-exercisesbody-subtitle "
         ).innerHTML = responce[i][2];
 
+        exercise_clone
+          .querySelector("#dashboard-create-modal-exercisesbody")
+          .setAttribute(
+            "id",
+            "dashboard-create-modal-exercise-" + responce[i][0]
+          );
+        exercise_clone
+          .querySelector(`#dashboard-create-modal-exercise-${responce[i][0]}`)
+          .setAttribute(
+            "onclick",
+            `on_exerciseclick(${responce[i][0]},'${responce[i][1]}','${responce[i][2]}')`
+          );
+
         //add themplate to the dom
         modalbody[0].appendChild(exercise_clone);
       }
     }
+    if (selectedexercise.length !== 0) {
+      $(
+        `#dashboard-create-modal-exercise-${selectedexercise[0]} #dashboard-create-modal-exercisesbody-title`
+      )
+        .css("color", "#0078e7")
+        .html(selectedexercise[1] + "<span> - selected</span>");
+    }
   });
 });
+
+function on_exerciseclick(id, name, type) {
+  //add the selected exersice to a selected list
+  selectedexercise = [];
+  selectedexercise = [id, name, type];
+  //reset styles on the exercise titles
+  $(".dashboard-create-modal-exercisesbody-title").css("color", "black");
+  $(".dashboard-create-modal-exercisesbody-title span").remove();
+
+  // add styles
+  console.log(selectedexercise[0]);
+  console.log(id);
+  if (selectedexercise[0].toString() === id.toString()) {
+    $(
+      `#dashboard-create-modal-exercise-${id.toString()} #dashboard-create-modal-exercisesbody-title`
+    )
+      .css("color", "#0078e7")
+      .html(name + "<span> - selected</span>");
+  }
+}
