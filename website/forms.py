130b7a03aa1a9a795e240740696f322910c9656e
squiddude38 <squiddude38@gmail.com>
2022-08-14 19:48:14 +1200

    edited forms file

--- website/forms.py
+++ website/forms.py
@@ -1,12 +1,10 @@
+# imports
 from flask_wtf import FlaskForm
-from werkzeug.security import check_password_hash
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
-from .models import User
-from flask import flash
-from flask_login import login_user
 
 
+# sign in form
 class SignInForm(FlaskForm):
     identifier = StringField('username or email', validators=[
                              DataRequired(), Length(min=0, max=200)], render_kw={"placeholder": "username or email"})
